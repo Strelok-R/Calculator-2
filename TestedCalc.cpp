863be716926e6990571302bdbc15e61fddf6230d
Strelok-R <72858444+Strelok-R@users.noreply.github.com>
2020-10-19 11:28:20 +0300

    Update TestedCalc.cpp

--- TestedCalc.cpp
+++ TestedCalc.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+bool unitTest() {
+    Calculator c;
+
+    cout << "-Check sum " << endl;
+    if (c.Calculate(10, '+', 10) != 20) return false;
+    cout << "-Check substraction " << endl;
+    if (c.Calculate(10, '-', 10) != 0) return false;
+    cout << "-Check multiplying " << endl;
+    if (c.Calculate(10, '*', 10) != 100) return false;
+    cout << "-Check division " << endl;
+    if (c.Calculate(10, '/', 10) != 1) return false;
+    return true;
+}
+
 int main()
 {
     bool test = false;
