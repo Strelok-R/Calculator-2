6033bf09c6774a5f89905228b7ff7dd89f806517
Strelok-R <72858444+Strelok-R@users.noreply.github.com>
2020-10-19 13:08:18 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,24 +1,24 @@
 #include "Calculator.h"
 #define CATCH_CONFIG_MAIN
 
-TEST_CASE("Test Amount")
+TEST_CASE ("Test Amount")
 {
     float expected = 6;
     REQUIRE(expected == sum(2, 4));
 }
-TEST_CASE("Test Difference")
+TEST_CASE ("Test Difference")
 {
     float expected = 7.5;
     REQUIRE(expected == diff(10, 2.5));
 }
 
-TEST_CASE("Test Multiplication")
+TEST_CASE ("Test Multiplication")
 {
     float expected = 8;
     REQUIRE(expected == multiP(2, 4));
 }
 
-TEST_CASE("Test Division")
+TEST_CASE ("Test Division")
 {
     float expected = 5;
     REQUIRE(expected == divs(10, 2));
