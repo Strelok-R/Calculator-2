2b642347b878506522d6419a8186e810d9684173
Strelok-R <72858444+Strelok-R@users.noreply.github.com>
2020-10-19 12:03:01 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,24 +2,25 @@
 #define CATCH_CONFIG_MAIN
 using namespace std;
 
-TEST_CASE("Test Sum")
+TEST_CASE("Test Amount")
 {
-	float expected = 6;
+    float expected = 6;
     REQUIRE(expected == sum(2, 4));
 }
-
-TEST_CASE("Test Differencen")
+TEST_CASE("Test Difference")
 {
- float expected = 7.5;
+    float expected = 7.5;
     REQUIRE(expected == diff(10, 2.5));
 }
+
 TEST_CASE("Test Multiplication")
 {
-	float expected = 8;
+    float expected = 8;
     REQUIRE(expected == multiP(2, 4));
 }
+
 TEST_CASE("Test Division")
 {
-  float expected = 5;
+    float expected = 5;
     REQUIRE(expected == divs(10, 2));
 }
