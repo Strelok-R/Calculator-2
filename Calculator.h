a441c7ecd3026688fb545880620bdb92bb7b43f7
Strelok-R <72858444+Strelok-R@users.noreply.github.com>
2020-10-19 13:51:04 +0300

    Update Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -1,6 +1,16 @@
-#pragma once
-class Calculator
-{
-public:
-    double Calculate(double x, char oper, double y);
-};
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void coutF(float x, float y, char oper, float z);
+
+float sum(float x, float y);
+
+float diff(float x, float y);
+
+float multiP(float x, float y);
+
+float divs(float x, float y);
